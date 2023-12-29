e80d41d4a6cee7d4b3b3780f9a70becc1073a9ce
MathieuGir <giraudmathieu@outlook.fr>
2023-12-29 23:48:31 +0100

    advanced on II)

---
+++ requirements.py
@@ -0,0 +1,15 @@
+"""
+Note des imports à faire :
+
+pip install pynsee [full]
+pip install matplotlib
+pip install seaborn
+pip install scipy
+pip instal numpy
+pip install -q lxml
+pip install bs4
+pip install webdriver-manager
+pip install statsmodel
+pip install --upgrade setuptools
+
+"""
